08d4f25f30c158b07d779a2e05ca55d728a398d5
branjbar <bij.ranjbar@gmail.com>
2014-06-23 18:39:57 +0200

    the degree distribution formulas corrected.

--- Matlab/unfirom_vs_strenght_base_comparison.m
+++ Matlab/unfirom_vs_strenght_base_comparison.m
@@ -1,21 +1,31 @@
+%% This code provides a quick overview of cumulative 
+%% degree distributions of both "uniform performance" and "strength-based performance" methods. 
+
 close all
 clear all
 clc
 
-deg1 = [];
-deg2 = [];
+deg1 = [];  % degree for uniform prefrence
+deg2 = [];  % degree for strenght-based prefrence
 N = 100000;
 
+%% uniform prefrence
 for i = 1: N
-    deg1(i) = 0;
-    deg2(i) = 1;
+    deg1(i) = 1;
     for j = i : N
         deg1(i) = deg1(i) + 1 / i;
-        deg2(i) = deg2(i) * (2*j + 1) / (2*j);
+    end
+end
+
+%% strenght-based prefrence
+for i = 1: N
+    deg2(i) = 1;
+    for j = i+1 : N+1
+        deg2(i) = deg2(i) * (2*j - 4) / (2*j-5);
     end
 end
         
-
+%% computing the cumulative distribution
 [y1, x1]= hist(deg1,100000);
 y1 = cumsum(y1(end:-1:1));
 y1 = y1(end:-1:1);
@@ -24,10 +34,12 @@ y1 = y1(end:-1:1);
 y2 = cumsum(y2(end:-1:1));
 y2 = y2(end:-1:1);
 
+%% plotting
 
-loglog(x1,y1,'.')
+loglog(x1,y1,'k.')
 hold on
-loglog(x2,y2,'r.')
+loglog(x2,y2,'r*')
 legend('uniform preference','strength-based prefrence')
 xlabel('degree (log-scale)')
 ylabel('cumulative frequency (log-scale)')
+xlim([1,10^5])
