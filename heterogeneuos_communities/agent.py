6bd5c805fca7bb7969597dff3def5eeb6af380d4
branjbar <bij.ranjbar@gmail.com>
2014-09-25 14:46:12 +0200

    agent class initialized

---
+++ heterogeneuos_communities/agent.py
@@ -0,0 +1,25 @@
+__author__ = 'Bijan'
+
+
+class Agent:
+    """
+    a general class for all our agents
+    """
+    id = None  # agent id = 0, 1, 2, ...
+    type = None  # agent type in {'GRAY', 'SMART'}
+    x = None  # agent behavior in [-infinity, +infinity]
+    fitness = None  # agent fitness in [-infinity, +infinity]
+    history = []  # agent history is a dictionary of dictionary in form of [{'x':x, 'fitness':fitness, 'time':time}]
+
+    def __init__(self, agent_id, type=None, x=None, fitness=None):
+        self.id = agent_id
+        self.type = type
+        self.x = x
+        self.fitness = fitness
+
+    def __str__(self):
+        return 'A_%s) %s: x=%s, F=%s' % (self.id, self.type, self.x, self.fitness)
+
+    def get_id(self):
+        return self.id
+
