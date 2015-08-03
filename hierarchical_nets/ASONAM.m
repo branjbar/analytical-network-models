e9e04acb5c054785f5a1fd6dc7efb422919be425
Bijan Ranjbar <bij.ranjbar@gmail.com>
2015-08-03 15:52:27 +0200

    Create ASONAM.m
    initialization

---
+++ hierarchical_nets/ASONAM.m
@@ -0,0 +1,11 @@
+%%Implementation of the ASONAM paper
+%% B. Ranjbar-Sahraei, H. Bou-Ammar, K. Tuyls, G. Weiss, "On the Skewed Degree Distribution of Hierarchical Networks", In Proceedings of the IEEE/ACM International Conference on Advances in Social Networks Analysis and Mining (ASONAM), Paris, France, 2015.
+
+% initialization
+
+% define the update rule
+
+% let the system evolve
+
+% visualize the network characteristics
+
