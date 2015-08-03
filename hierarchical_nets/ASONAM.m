25d02e9fc6961835bd08afeb879882626ff19d23
Bijan Ranjbar <bij.ranjbar@gmail.com>
2015-08-03 16:31:20 +0200

    Prestige-based attachment model

--- hierarchical_nets/ASONAM.m
+++ hierarchical_nets/ASONAM.m
@@ -1,11 +1,28 @@
 %%Implementation of the ASONAM paper
-%% B. Ranjbar-Sahraei, H. Bou-Ammar, K. Tuyls, G. Weiss, "On the Skewed Degree Distribution of Hierarchical Networks", In Proceedings of the IEEE/ACM International Conference on Advances in Social Networks Analysis and Mining (ASONAM), Paris, France, 2015.
+% Reference B. Ranjbar-Sahraei, H. Bou-Ammar, K. Tuyls, G. Weiss, "On the Skewed Degree Distribution of Hierarchical Networks", In Proceedings of the IEEE/ACM International Conference on Advances in Social Networks Analysis and Mining (ASONAM), Paris, France, 2015.
 
-% initialization
+%% initialization
+n = 10;  % number of nodes
+W = zeros(n,n);  % the triangular weighted adjacency matrix 
+Time = 10;  % number of evolution iterations
+deltaT = .1;
+W(1,1) = 1;  % initialize a link between node 2 and node 1
+%% define the update rule
+clc
+for t = 1 : Time
+    Psi = W;
+    
+    for i = 1 : n
+        Psi = Psi + [zeros(n,i), W(:,1:n-i)];
+    end
+    
+    dW = Psi ./ (sum(Psi,2) * ones(1,n));
+    W = W + dW * deltaT;
+end
 
-% define the update rule
+W
+   
 
-% let the system evolve
+%% let the system evolve
 
-% visualize the network characteristics
-
+%% visualize the network characteristics
