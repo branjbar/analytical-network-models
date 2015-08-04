c89373846fa96ec1d8ccfa7698e3e61add31e3d4
Bijan Ranjbar <bij.ranjbar@gmail.com>
2015-08-04 14:41:51 +0200

    Create generic_model.m

---
+++ hierarchical_nets/generic_model.m
@@ -0,0 +1,6 @@
+%%Generic Model: I have a generic model for constructing hierarchical
+%%models. first, an arbitrary hierarchy structury is chosen, and then the
+%%update rules are generated. Based on update rules the edge weights are
+%%evolved and the evolution is demonstrated in real time.
+
+
