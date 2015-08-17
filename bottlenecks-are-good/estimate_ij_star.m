d181ca39d16a34eedac8a99c064f7fd4417d3838
branjbar <bij.ranjbar@gmail.com>
2015-08-17 18:07:29 +0200

    documentation improved.

--- bottlenecks-are-good/estimate_ij_star.m
+++ bottlenecks-are-good/estimate_ij_star.m
@@ -1,9 +1,14 @@
 function [I,J] = estimate_ij_star(W, plot_flag)
+% Estimate IJ STAR
+% Estimate sizes of $F_{I^*}$ and $J^*$. The former one is equal to the
+% number of sensors needed to decouple disturbances and the latter one is
+% the number of nodes whitin the community which can be controlled.
+
         n = size(W,1);
         INF = zeros(floor(n/10),n,n);
-        for d = 1 : 15
+        for d = 1 : 20  % for distance between u and y equal to d
             INF(d,:,:) = (W')^d;
-            for i = 1 : n
+            for i = 1 : n  % for every node
                 
                 if d > 1
                     J(d,i) = 1 + nnz(sum(INF(1:d-1,i,:),1));
@@ -22,16 +27,14 @@ function [I,J] = estimate_ij_star(W, plot_flag)
     I = mean(I,2);
     J = mean(J,2);
 
-    
+
     if plot_flag
         plot(I,'.:')
         hold on
         plot(J)    
-        legend('F_{I^*}','J^*','Location','NorthWest')
+        legend('|F_{I^*}|','|J^*|','Location','NorthWest')
         xlabel('L(u-->y)')
         ylabel('size')
-%         ylim([1,n/2])
-%         xlim([1,size(I,1)])
 
     end
 end
